c098957f0a163c75c1a526d9748bc10af61fc0ce
Jesse <jesse.chen26@myhunter.cuny.edu>
2025-03-18 15:29:28 -0400

    initial changes to fillAll

--- script.js
+++ script.js
@@ -93,7 +93,10 @@ function fillU(){
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    let cells = document.querySelectorAll('td');
+    for (let i = 0; i<cells.length; i++) {
+        cells[i].style.backgroundColor = colorSelected;
+    }
 }
 
 // Clear all cells
